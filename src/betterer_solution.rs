119d37c6c5190e74fd54ff9a09bfa80b4b3ed174
Jonathan Jayet <jonathan@artus.io>
2017-07-07 15:42:53 +0200

    Added another solution

--- src/betterer_solution.rs
+++ src/betterer_solution.rs
@@ -1,40 +1,38 @@
-pub mod better_solution {
+pub mod betterer_solution {
     // This is O(N) space and O(N) time complexity
     #[derive(Debug)]
-    pub struct BetterSolution {
+    pub struct BettererSolution {
         vec: Vec<i32>,
-        left_vec: Vec<i32>,
-        right_vec: Vec<i32>,
+        diff_vec: Vec<i32>,
     }
 
-    impl BetterSolution {
+    impl BettererSolution {
         fn calculate_left_highest_points(&mut self) {
             let mut current_highest_point = 0;
             for (i, &item) in self.vec.iter().enumerate() {
                 if item > current_highest_point {
                     current_highest_point = item
                 }
-                self.left_vec[i] = current_highest_point;
+                self.diff_vec[i] = current_highest_point
             }
         }
 
         fn calculate_right_highest_points(&mut self) {
-            let mut current_highest_point = self.vec[self.vec.len() - 1];
+            let mut current_highest_point = 0;
 
             for i in (0..self.vec.len()).rev() {
                 if current_highest_point < self.vec[i] {
                     current_highest_point = self.vec[i]
                 }
-                self.right_vec[i] = current_highest_point;
+                if current_highest_point < self.diff_vec[i] {
+                    self.diff_vec[i] = current_highest_point
+                }
             }
+
         }
 
-        fn get_stored_rain(&self, left: i32, right: i32, current: i32) -> i32 {
-            if left < right {
-                left - current
-            } else {
-                right - current
-            }
+        fn get_stored_rain(&self, diff: i32, current: i32) -> i32 {
+            diff - current
         }
 
         pub fn compute(&mut self) -> i32 {
@@ -44,25 +42,20 @@ pub mod better_solution {
             self.calculate_right_highest_points();
 
             for (index, &current_item) in self.vec.iter().enumerate() {
-                total_stored_rain +=
-                    self.get_stored_rain(self.left_vec[index], self.right_vec[index], current_item);
+                total_stored_rain += self.get_stored_rain(self.diff_vec[index], current_item);
             }
 
             total_stored_rain
         }
 
-        pub fn new(vec: Vec<i32>) -> BetterSolution {
+        pub fn new(vec: Vec<i32>) -> BettererSolution {
             let length = vec.capacity();
-            let mut bs = BetterSolution {
+            let mut bs = BettererSolution {
                 vec: vec,
-                left_vec: Vec::with_capacity(length),
-                right_vec: Vec::with_capacity(length),
+                diff_vec: Vec::with_capacity(length),
             };
 
-            unsafe {
-                bs.left_vec.set_len(length);
-                bs.right_vec.set_len(length);
-            }
+            unsafe { bs.diff_vec.set_len(length) }
 
             bs
         }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
+#[macro_use]
+extern crate timeit;
+
 mod naive_solution;
 mod better_solution;
+mod betterer_solution;
 
 use naive_solution::naive_solution::NaiveSolution;
 use better_solution::better_solution::BetterSolution;
+use betterer_solution::betterer_solution::BettererSolution;
 
 fn main() {
     println!("This is the rainfall problem");
     println!("Basically, you want to calculate the amount of water than can be stored between two mountains");
     println!("An array will represent the mountains height :");
-    let vec = vec![4, 5, 3, 2, 3, 6, 3, 2, 4, 3];
+    let vec = vec![4, 5, 3, 2, 3, 6, 3, 2, 4, 3, 5, 324, 3, 320];
     println!("{:?}", vec);
 
     let naive_solution = NaiveSolution {
         vec : vec.clone()
     };
 
-    println!("Stored rain in naive solution : {}", naive_solution.compute());
+    timeit!({
+        //println!("Stored rain in naive solution : {}", naive_solution.compute());
+        assert!(337 == naive_solution.compute())
+    });
 
     let mut better_solution = BetterSolution::new(vec.clone());
-
-    println!("Stored rain in better solution : {}", better_solution.compute());
+    timeit!({
+        //println!("Stored rain in better solution : {}", better_solution.compute());
+        assert!(337 == better_solution.compute())
+    });
+   
+    let mut betterer_solution = BettererSolution::new(vec);
+    timeit!({
+        //println!("Stored rain in betterer solution : {}", betterer_solution.compute());
+        assert!(337 == betterer_solution.compute())
+    });
 }
